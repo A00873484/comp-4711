e851f4a97f849feaa41e365f19d70f7b77fe8d36
Jay Huang <torvalds@linux-foundation.org>
2014-09-28 22:57:20 -0700

    Updated single.php

--- assignment1/application/views/single.php
+++ assignment1/application/views/single.php
@@ -1,4 +1,3 @@
-
 <html class="no-js" lang="en-US" id="ls-global">
 <head>
 	<meta charset="UTF-8">
@@ -12,28 +11,28 @@
 	================================================== -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-	<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/foundation-responsive.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/style-custom.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/style.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/foundation-responsive.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/style-custom.css" type="text/css" media="all">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Sans%3A100%2C300%2Cn%2Ci%2Cb%2Cbi&subset=latin" type="text/css" media="all">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Serif%3A100%2C300%2Cn%2Ci%2Cb%2Cbi&subset=latin" type="text/css" media="all">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans%3A100%2C300%2Cn%2Ci%2Cb%2Cbi&subset=latin" type="text/css" media="all">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto+Condensed%3A100%2C300%2Cn%2Ci%2Cb%2Cbi&subset=latin" type="text/css" media="all">
-	<link rel="stylesheet" href="css/layerslider.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/superfish.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/fancybox.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/jquery.fancybox-thumbs.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/font-awesome.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/jquery-ui-1.8.16.custom.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all">
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/jquery-migrate.min.js"></script>
-	<script type="text/javascript" src="js/jquery.gmap.min.js"></script>
-	<script type="text/javascript" src="js/layerslider.kreaturamedia.jquery.js"></script>
-	<script type="text/javascript" src="js/jquery-easing-1.3.js"></script>
-	<script type="text/javascript" src="js/jquerytransit.js"></script>
-	<script type="text/javascript" src="js/layerslider.transitions.js"></script>
-	<link rel="stylesheet" href="css/skin.css" type="text/css">
+	<link rel="stylesheet" href="/css/layerslider.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/superfish.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/fancybox.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/jquery.fancybox-thumbs.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/font-awesome.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/jquery-ui-1.8.16.custom.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/css/flexslider.css" type="text/css" media="all">
+	<script type="text/javascript" src="/js/jquery.js"></script>
+	<script type="text/javascript" src="/js/jquery-migrate.min.js"></script>
+	<script type="text/javascript" src="/js/jquery.gmap.min.js"></script>
+	<script type="text/javascript" src="/js/layerslider.kreaturamedia.jquery.js"></script>
+	<script type="text/javascript" src="/js/jquery-easing-1.3.js"></script>
+	<script type="text/javascript" src="/js/jquerytransit.js"></script>
+	<script type="text/javascript" src="/js/layerslider.transitions.js"></script>
+	<link rel="stylesheet" href="/css/skin.css" type="text/css">
 </head>
 <body class="home page page-template-default">
 	<div class="body-outer-wrapper ">
@@ -69,7 +68,7 @@
 												</a>
 											</li>
 											<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children">
-												<a href="/explore/" class="sf-with-ul">
+												<a href="/explore" class="sf-with-ul">
 													Explore
 												</a>
 											</li>
@@ -154,18 +153,90 @@
 							}
 						});
 						</script>
+						<div id="layerslider_1" class="ls-wp-container ls-container ls-packagetour" style="margin: 0px auto; visibility: visible; width: 100%; height: 560px;">
+							<div class="ls-webkit-hack"></div>
+							<div class="ls-inner" style="width: 1903px; height: 560px;">
+								<div class="ls-layer" style="left: auto; right: 0px; top: 0px; bottom: auto; width: 1903px; height: 560px; display: none; visibility: visible;">
+									<img src="/img/15373704942_704658a050_h.jpg" class="ls-bg" style="padding: 0px; border-width: 0px; width: 1903px; height: 860px; margin-left: -951.5px; margin-top: -430px;">
+									<div class="ls-s-1" style="position: absolute; top:230px; left: 50px; slidedirection : fade; slideoutdirection : fade; durationin : 2000; durationout : 1000; easingin : easeInOutQuint; easingout : easeInOutQuint; delayin : 0; delayout : 0; showuntil : 0; font-size: 60px; font-family: 'Open Sans'; color: #1c5b6d; font-weight: 100;  white-space: nowrap;">Japan</div>
+									<div class="ls-s-1" style="position: absolute; top:300px; left: 650px;  slidedirection : top; durationin: 2500; delayin : 500; delayout : 0; showuntil : 0; font-size: 42px; font-family: 'Open Sans'; color: #1c5b6d; font-weight: 100; white-space: nowrap;">Land of the rising sun</div>
+								</div>
+								<div class="ls-layer" style="width: 1903px; height: 560px;">
+									<img src="/img/15278354632_ea0408bee3_o.jpg" class="ls-bg">
+									<div class="ls-s-1" style="position: absolute; font-family: 'Open Sans'; color: #FFF; font-weight: 100; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-right-radius: 5px; border-bottom-left-radius: 5px; white-space: nowrap; width: auto; height: auto; font-size: 36px; padding: 5px 15px; border-width: 0px; left: -281.5px; top: 146px;">A mix of ancient and modern</div>
+									<div class="ls-s-1" style="position: absolute; top:280px; left: -110px;  slidedirection : top; durationin: 1500; delayin : 0; delayout : 0; showuntil : 0; font-size: 30px; font-family: 'Open Sans'; color: #FFF; font-weight: 100; white-space: nowrap;">Experience the wonder</div>
+								</div>
+								<div class="ls-layer" style="display: none; visibility: visible; width: 1903px; height: 560px; left: 0px; right: auto; top: 0px; bottom: auto;">
+									<img src="/img/15153987320_de7c939055_o.jpg" class="ls-bg" style="padding: 0px; border-width: 0px; width: 1903px; height: 860px; margin-left: -951.5px; margin-top: -430px;">
+									<div class="ls-s-1" style="position: absolute; font-family: 'Open Sans'; color: rgb(255, 255, 255); font-weight: 100; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-right-radius: 5px; border-bottom-left-radius: 5px; white-space: nowrap; width: auto; height: auto; font-size: 36px; padding: 5px 15px; border-width: 0px; left: 591.5px; top: 166px; background: rgba(0, 0, 0, 0.701961);">Amazing food, culture, and beauty</div>
+								</div>
+								<div class="ls-circle-timer">
+									<div class="ls-ct-left">
+										<div class="ls-ct-rotate" style="transform: rotate(0deg);">
+											<div class="ls-ct-hider">
+												<div class="ls-ct-half"></div>
+											</div>
+										</div>
+									</div>
+									<div class="ls-ct-right">
+										<div class="ls-ct-rotate" style="transform: rotate(0deg);">
+											<div class="ls-ct-hider">
+												<div class="ls-ct-half"></div>
+											</div>
+										</div>
+									</div>
+									<div class="ls-ct-center"></div>
+								</div>
+							</div>
+							<div class="ls-loading-container" style="z-index: -1; display: none;">
+								<div class="ls-loading-indicator"></div>
+							</div>
+						</div>
+						<div class="clear"></div>
 					</div>
 				</div>
-				
 			</div>
 			<!-- header outer wrapper -->
 			<div class="content-outer-wrapper">
-				
-				<div class="package-thumbnail-outer-wrapper">
+				<div class="page-full-wrapper">
+					<div>
+						<div class="page-wrapper single-page ">
+							<div class="gdl-page-item">
+								<div class="container">
+									<div class="row">
+										<div class="twelve columns mb0">
+											<div class="gdl-page-content"></div>
+										</div>
+										<div class="clear"></div>
+									</div>
+								</div>
+								<div class="container">
+									<div class="row">
+										<div class="twelve columns">
+											<div class="title-item-wrapper">
+												<div class="title-item-gimmick left" style="width: 356px;"></div>
+												<h2 class="title-item-header">
+													<span>Play in Japan</span>
+												</h2>
+												<div class="title-item-gimmick right" style="width: 356px;"></div>
+											</div>
+										</div>
+										<div class="clear"></div>
+									</div>
+								</div>
+								<div class="container">
+									<div class="row">
+										<div class="twelve columns package-item-class package-item-class-1">
+											<div class="package-item-holder">
+												<div class="row">
+													<div class="twelve columns gdl-package-grid2">
+														<div class="package-content-wrapper">
+															<div class="package-thumbnail-outer-wrapper">
 																<div class="package-media-wrapper gdl-image">
 																	<a href="{href}">
 																		<img src="/img/{image}" alt="">
-																		<div class="package-ribbon-wrapper"> 
+																		<div class="package-ribbon-wrapper">
+																			<div class="package-type normal-type">Learn More</div>
 																			<div class="clear"></div>
 																			<div class="package-type-gimmick"></div>
 																			<div class="clear"></div>
@@ -175,12 +246,24 @@
 																<div class="package-title-wrapper">
 																	<div class="package-title-overlay"></div>
 																	<h2 class="package-title">
-																		<div >{name}<br><br>{description}</div>
+																		<a>{name}<br><br>{description}</a>
 																	</h2>
 																</div>
 															</div>
-				
-				
+														</div>
+													</div>
+													<div class="clear"></div>
+												</div>
+											</div>
+											<div class="clear"></div>
+										</div>
+										<div class="clear"></div>
+									</div>
+								</div>
+						<!-- page wrapper -->
+					</div>
+					<!-- post class -->
+				</div>
 				<!-- page-full-wrapper -->
 			</div>
 			<div class="footer-wrapper boxed-style">
@@ -203,32 +286,27 @@
 	</div>
 	<!-- body outer wrapper -->
 
-	<script type="text/javascript">jQuery(document).ready(function(){});</script>
-	<script>
-var getElementsByClassName=function(a,b,c){if(document.getElementsByClassName){getElementsByClassName=function(a,b,c){c=c||document;var d=c.getElementsByClassName(a),e=b?new RegExp("\\b"+b+"\\b","i"):null,f=[],g;for(var h=0,i=d.length;h<i;h+=1){g=d[h];if(!e||e.test(g.nodeName)){f.push(g)}}return f}}else if(document.evaluate){getElementsByClassName=function(a,b,c){b=b||"*";c=c||document;var d=a.split(" "),e="",f="http://www.w3.org/1999/xhtml",g=document.documentElement.namespaceURI===f?f:null,h=[],i,j;for(var k=0,l=d.length;k<l;k+=1){e+="[contains(concat(' ', @class, ' '), ' "+d[k]+" ')]"}try{i=document.evaluate(".//"+b+e,c,g,0,null)}catch(m){i=document.evaluate(".//"+b+e,c,null,0,null)}while(j=i.iterateNext()){h.push(j)}return h}}else{getElementsByClassName=function(a,b,c){b=b||"*";c=c||document;var d=a.split(" "),e=[],f=b==="*"&&c.all?c.all:c.getElementsByTagName(b),g,h=[],i;for(var j=0,k=d.length;j<k;j+=1){e.push(new RegExp("(^|\\s)"+d[j]+"(\\s|$)"))}for(var l=0,m=f.length;l<m;l+=1){g=f[l];i=false;for(var n=0,o=e.length;n<o;n+=1){i=e[n].test(g.className);if(!i){break}}if(i){h.push(g)}}return h}}return getElementsByClassName(a,b,c)},
-dropdowns = getElementsByClassName( 'dropdown-menu' );
-for ( i=0; i<dropdowns.length; i++ )
-	dropdowns[i].onchange = function(){ if ( this.value != '' ) window.location.href = this.value; }
-</script>
-	<script type="text/javascript" src="js/superfish.js"></script>
-	<script type="text/javascript" src="js/supersub.js"></script>
-	<script type="text/javascript" src="js/hoverIntent.js"></script>
-	<script type="text/javascript" src="js/jquery.easing.js"></script>
 	<script type="text/javascript">
-	/* <![CDATA[ */
-	var ATTR = {"enable":"enable","width":"80","height":"45"};
-	/* ]]>*/</script>
-	<script type="text/javascript" src="js/jquery.fancybox.js"></script>
-	<script type="text/javascript" src="js/jquery.fancybox-media.js"></script>
-	<script type="text/javascript" src="js/jquery.fancybox-thumbs.js"></script>
-	<script type="text/javascript" src="js/gdl-scripts.js"></script>
-	<script type="text/javascript" src="js/jquery.ui.core.min.js"></script>
-	<script type="text/javascript" src="js/jquery.cycle.js"></script>
-	<script type="text/javascript" src="js/blog-carousel.js"></script>
-	<script type="text/javascript">
-	/* <![CDATA[ */
-	var FLEX = {"animation":"fade","pauseOnHover":"enable","controlNav":"disable","directionNav":"enable","animationSpeed":"600","slideshowSpeed":"12000","pauseOnAction":"disable","thumbnail_width":"75","thumbnail_height":"50","controlsContainer":".flexslider"};
-	/* ]]>*/</script>
-	<script type="text/javascript" src="js/jquery.flexslider.js"></script>
+		var ATTR = {"enable":"enable","width":"80","height":"45"};
+		var FLEX = {"animation":"fade","pauseOnHover":"enable","controlNav":"disable","directionNav":"enable","animationSpeed":"600","slideshowSpeed":"12000","pauseOnAction":"disable","thumbnail_width":"75","thumbnail_height":"50","controlsContainer":".flexslider"};
+		jQuery(document).ready(function(){
+			var getElementsByClassName=function(a,b,c){if(document.getElementsByClassName){getElementsByClassName=function(a,b,c){c=c||document;var d=c.getElementsByClassName(a),e=b?new RegExp("\\b"+b+"\\b","i"):null,f=[],g;for(var h=0,i=d.length;h<i;h+=1){g=d[h];if(!e||e.test(g.nodeName)){f.push(g)}}return f}}else if(document.evaluate){getElementsByClassName=function(a,b,c){b=b||"*";c=c||document;var d=a.split(" "),e="",f="http://www.w3.org/1999/xhtml",g=document.documentElement.namespaceURI===f?f:null,h=[],i,j;for(var k=0,l=d.length;k<l;k+=1){e+="[contains(concat(' ', @class, ' '), ' "+d[k]+" ')]"}try{i=document.evaluate(".//"+b+e,c,g,0,null)}catch(m){i=document.evaluate(".//"+b+e,c,null,0,null)}while(j=i.iterateNext()){h.push(j)}return h}}else{getElementsByClassName=function(a,b,c){b=b||"*";c=c||document;var d=a.split(" "),e=[],f=b==="*"&&c.all?c.all:c.getElementsByTagName(b),g,h=[],i;for(var j=0,k=d.length;j<k;j+=1){e.push(new RegExp("(^|\\s)"+d[j]+"(\\s|$)"))}for(var l=0,m=f.length;l<m;l+=1){g=f[l];i=false;for(var n=0,o=e.length;n<o;n+=1){i=e[n].test(g.className);if(!i){break}}if(i){h.push(g)}}return h}}return getElementsByClassName(a,b,c)},
+			dropdowns = getElementsByClassName( 'dropdown-menu' );
+			for ( i=0; i<dropdowns.length; i++ )
+				dropdowns[i].onchange = function(){ if ( this.value != '' ) window.location.href = this.value; }
+		});
+	</script>
+	<script type="text/javascript" defer src="/js/superfish.js"></script>
+	<script type="text/javascript" defer src="/js/supersub.js"></script>
+	<script type="text/javascript" defer src="/js/hoverIntent.js"></script>
+	<script type="text/javascript" defer src="/js/jquery.easing.js"></script>
+	<script type="text/javascript" defer src="/js/jquery.fancybox.js"></script>
+	<script type="text/javascript" defer src="/js/jquery.fancybox-media.js"></script>
+	<script type="text/javascript" defer src="/js/jquery.fancybox-thumbs.js"></script>
+	<script type="text/javascript" defer src="/js/gdl-scripts.js"></script>
+	<script type="text/javascript" defer src="/js/jquery.ui.core.min.js"></script>
+	<script type="text/javascript" defer src="/js/jquery.cycle.js"></script>
+	<script type="text/javascript" defer src="/js/blog-carousel.js"></script>
+	<script type="text/javascript" defer src="/js/jquery.flexslider.js"></script>
 </body>
 </html>
