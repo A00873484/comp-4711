eb52f4a89233d82cfaac12579c55925dfbbc111f
Daniel <danny.engelhard@gmail.com>
2014-11-02 14:23:04 -0800

    admin pictures showing but added code 91-108 in admin dosnt work

--- assignment1/application/views/_fields/x_submitted.php
+++ assignment1/application/views/_fields/x_submitted.php
@@ -2,7 +2,8 @@
     <label class="control-label" >Already uploaded</label>
     <div class="controls">
         <div class="fileupload fileupload-new" data-provides="fileupload">
-            Image: <img src="/assets/images/{name}" alt="{label}" border="0" width="{width} height={height}"/>
+            Image: <img src="/img/{name}" alt="{label}" border="0" width="{width} height={height}"/>
+            <input name="{label}" type="file">
         </div>
     </div>
 </div>
