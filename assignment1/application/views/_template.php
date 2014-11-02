a46747cbbb4eff9dd5d37e27415f9926874e324d
Jay Huang <torvalds@linux-foundation.org>
2014-11-02 14:38:57 -0800

    Removed extraneous JS code, added defer, styling fixes

--- assignment1/application/views/_template.php
+++ assignment1/application/views/_template.php
@@ -12,6 +12,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 	<link rel="stylesheet" href="/css/style.css" type="text/css" media="all">
+	<!-- <link rel="stylesheet" href="/css/foundation.css" type="text/css"> -->
 	<link rel="stylesheet" href="/css/foundation-responsive.css" type="text/css" media="all">
 	<link rel="stylesheet" href="/css/style-custom.css" type="text/css" media="all">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Sans%3A100%2C300%2Cn%2Ci%2Cb%2Cbi&subset=latin" type="text/css" media="all">
@@ -25,14 +26,14 @@
 	<link rel="stylesheet" href="/css/font-awesome.css" type="text/css" media="all">
 	<link rel="stylesheet" href="/css/jquery-ui-1.8.16.custom.css" type="text/css" media="all">
 	<link rel="stylesheet" href="/css/flexslider.css" type="text/css" media="all">
-	<script type="text/javascript" src="/js/jquery.js"></script>
-	<script type="text/javascript" src="/js/jquery-migrate.min.js"></script>
-	<script type="text/javascript" src="/js/jquery.gmap.min.js"></script>
-	<script type="text/javascript" src="/js/layerslider.kreaturamedia.jquery.js"></script>
-	<script type="text/javascript" src="/js/jquery-easing-1.3.js"></script>
-	<script type="text/javascript" src="/js/jquerytransit.js"></script>
-	<script type="text/javascript" src="/js/layerslider.transitions.js"></script>
 	<link rel="stylesheet" href="/css/skin.css" type="text/css">
+	<script type="text/javascript" src="/js/jquery.js"></script>
+	<script type="text/javascript" defer src="/js/jquery-migrate.min.js"></script>
+	<script type="text/javascript" defer src="/js/jquery.gmap.min.js"></script>
+	<script type="text/javascript" defer src="/js/layerslider.kreaturamedia.jquery.js"></script>
+	<script type="text/javascript" defer src="/js/jquery-easing-1.3.js"></script>
+	<script type="text/javascript" defer src="/js/jquerytransit.js"></script>
+	<script type="text/javascript" defer src="/js/layerslider.transitions.js"></script>
 </head>
 <body class="home page page-template-default">
 	<div class="body-outer-wrapper ">
@@ -42,7 +43,28 @@
             {content}
 			{footer}
 		</div>
-    <script src="/assets/js/jquery-1.11.1.min.js"></script>
     <script src="/assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    	var ATTR = {"enable":"enable","width":"80","height":"45"};
+    	var FLEX = {"animation":"fade","pauseOnHover":"enable","controlNav":"disable","directionNav":"enable","animationSpeed":"600","slideshowSpeed":"12000","pauseOnAction":"disable","thumbnail_width":"75","thumbnail_height":"50","controlsContainer":".flexslider"};
+    	jQuery(document).ready(function(){
+    		var getElementsByClassName=function(a,b,c){if(document.getElementsByClassName){getElementsByClassName=function(a,b,c){c=c||document;var d=c.getElementsByClassName(a),e=b?new RegExp("\\b"+b+"\\b","i"):null,f=[],g;for(var h=0,i=d.length;h<i;h+=1){g=d[h];if(!e||e.test(g.nodeName)){f.push(g)}}return f}}else if(document.evaluate){getElementsByClassName=function(a,b,c){b=b||"*";c=c||document;var d=a.split(" "),e="",f="http://www.w3.org/1999/xhtml",g=document.documentElement.namespaceURI===f?f:null,h=[],i,j;for(var k=0,l=d.length;k<l;k+=1){e+="[contains(concat(' ', @class, ' '), ' "+d[k]+" ')]"}try{i=document.evaluate(".//"+b+e,c,g,0,null)}catch(m){i=document.evaluate(".//"+b+e,c,null,0,null)}while(j=i.iterateNext()){h.push(j)}return h}}else{getElementsByClassName=function(a,b,c){b=b||"*";c=c||document;var d=a.split(" "),e=[],f=b==="*"&&c.all?c.all:c.getElementsByTagName(b),g,h=[],i;for(var j=0,k=d.length;j<k;j+=1){e.push(new RegExp("(^|\\s)"+d[j]+"(\\s|$)"))}for(var l=0,m=f.length;l<m;l+=1){g=f[l];i=false;for(var n=0,o=e.length;n<o;n+=1){i=e[n].test(g.className);if(!i){break}}if(i){h.push(g)}}return h}}return getElementsByClassName(a,b,c)},
+    		dropdowns = getElementsByClassName( 'dropdown-menu' );
+    		for ( i=0; i<dropdowns.length; i++ )
+    			dropdowns[i].onchange = function(){ if ( this.value != '' ) window.location.href = this.value; }
+    	});
+    </script>
+    <script type="text/javascript" defer src="/js/superfish.js"></script>
+    <script type="text/javascript" defer src="/js/supersub.js"></script>
+    <script type="text/javascript" defer src="/js/hoverIntent.js"></script>
+    <script type="text/javascript" defer src="/js/jquery.easing.js"></script>
+    <script type="text/javascript" defer src="/js/jquery.fancybox.js"></script>
+    <script type="text/javascript" defer src="/js/jquery.fancybox-media.js"></script>
+    <script type="text/javascript" defer src="/js/jquery.fancybox-thumbs.js"></script>
+    <script type="text/javascript" defer src="/js/gdl-scripts.js"></script>
+    <script type="text/javascript" defer src="/js/jquery.ui.core.min.js"></script>
+    <script type="text/javascript" defer src="/js/jquery.cycle.js"></script>
+    <script type="text/javascript" defer src="/js/blog-carousel.js"></script>
+    <script type="text/javascript" defer src="/js/jquery.flexslider.js"></script>
 </body>
 </html>
--- assignment1/application/views/exploreview.php
+++ assignment1/application/views/exploreview.php
@@ -112,28 +112,5 @@
 		<!-- body wrapper -->
 	</div>
 	<!-- body outer wrapper -->
-
-	<script type="text/javascript">
-		var ATTR = {"enable":"enable","width":"80","height":"45"};
-		var FLEX = {"animation":"fade","pauseOnHover":"enable","controlNav":"disable","directionNav":"enable","animationSpeed":"600","slideshowSpeed":"12000","pauseOnAction":"disable","thumbnail_width":"75","thumbnail_height":"50","controlsContainer":".flexslider"};
-		jQuery(document).ready(function(){
-			var getElementsByClassName=function(a,b,c){if(document.getElementsByClassName){getElementsByClassName=function(a,b,c){c=c||document;var d=c.getElementsByClassName(a),e=b?new RegExp("\\b"+b+"\\b","i"):null,f=[],g;for(var h=0,i=d.length;h<i;h+=1){g=d[h];if(!e||e.test(g.nodeName)){f.push(g)}}return f}}else if(document.evaluate){getElementsByClassName=function(a,b,c){b=b||"*";c=c||document;var d=a.split(" "),e="",f="http://www.w3.org/1999/xhtml",g=document.documentElement.namespaceURI===f?f:null,h=[],i,j;for(var k=0,l=d.length;k<l;k+=1){e+="[contains(concat(' ', @class, ' '), ' "+d[k]+" ')]"}try{i=document.evaluate(".//"+b+e,c,g,0,null)}catch(m){i=document.evaluate(".//"+b+e,c,null,0,null)}while(j=i.iterateNext()){h.push(j)}return h}}else{getElementsByClassName=function(a,b,c){b=b||"*";c=c||document;var d=a.split(" "),e=[],f=b==="*"&&c.all?c.all:c.getElementsByTagName(b),g,h=[],i;for(var j=0,k=d.length;j<k;j+=1){e.push(new RegExp("(^|\\s)"+d[j]+"(\\s|$)"))}for(var l=0,m=f.length;l<m;l+=1){g=f[l];i=false;for(var n=0,o=e.length;n<o;n+=1){i=e[n].test(g.className);if(!i){break}}if(i){h.push(g)}}return h}}return getElementsByClassName(a,b,c)},
-			dropdowns = getElementsByClassName( 'dropdown-menu' );
-			for ( i=0; i<dropdowns.length; i++ )
-				dropdowns[i].onchange = function(){ if ( this.value != '' ) window.location.href = this.value; }
-		});
-	</script>
-	<script type="text/javascript" defer src="js/superfish.js"></script>
-	<script type="text/javascript" defer src="js/supersub.js"></script>
-	<script type="text/javascript" defer src="js/hoverIntent.js"></script>
-	<script type="text/javascript" defer src="js/jquery.easing.js"></script>
-	<script type="text/javascript" defer src="js/jquery.fancybox.js"></script>
-	<script type="text/javascript" defer src="js/jquery.fancybox-media.js"></script>
-	<script type="text/javascript" defer src="js/jquery.fancybox-thumbs.js"></script>
-	<script type="text/javascript" defer src="js/gdl-scripts.js"></script>
-	<script type="text/javascript" defer src="js/jquery.ui.core.min.js"></script>
-	<script type="text/javascript" defer src="js/jquery.cycle.js"></script>
-	<script type="text/javascript" defer src="js/blog-carousel.js"></script>
-	<script type="text/javascript" defer src="js/jquery.flexslider.js"></script>
 </body>
 </html>
